e8f9734b6b2120788a796914fb25d3840628849b
Prashant Koshta <prashantkoshta@gmail.com>
2016-10-16 18:25:17 -0500

    Added icon genrator for app.

---
+++ background.bat
@@ -0,0 +1,13 @@
+@REM Android
+call svgexport background.svg App_Resources/Android/drawable-hdpi/background.png pad 576:768
+call svgexport background.svg App_Resources/Android/drawable-ldpi/background.png pad 288:384
+call svgexport background.svg App_Resources/Android/drawable-mdpi/background.png pad 384:512
+call svgexport background.svg App_Resources/Android/drawable-xhdpi/background.png pad 768:1024
+call svgexport background.svg App_Resources/Android/drawable-xxhdpi/background.png pad 1152:1536
+call svgexport background.svg App_Resources/Android/drawable-xxxhdpi/background.png pad 1536:2048
+
+
+
+@REM iOS
+call svgexport background.svg App_Resources/iOS/Assets.xcassets/LaunchScreen.AspectFill.imageset/LaunchScreen-AspectFill.png pad 768:1024
+call svgexport background.svg App_Resources/iOS/Assets.xcassets/LaunchScreen.AspectFill.imageset/LaunchScreen-AspectFill@2x.png pad 1536:2048
---
+++ icon.bat
@@ -0,0 +1,40 @@
+@REM Android
+call svgexport icon.svg App_Resources/Android/drawable-hdpi/icon.png pad 72:72
+call svgexport icon.svg App_Resources/Android/drawable-ldpi/icon.png pad 36:36
+call svgexport icon.svg App_Resources/Android/drawable-mdpi/icon.png pad 48:48
+call svgexport icon.svg App_Resources/Android/drawable-xhdpi/icon.png pad 96:96
+call svgexport icon.svg App_Resources/Android/drawable-xxhdpi/icon.png pad 144:144
+call svgexport icon.svg App_Resources/Android/drawable-xxxhdpi/icon.png pad 345:345
+
+@REM iOS
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-29.png pad 29:29
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-29@2x.png pad 58:58
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-29@3x.png pad 87:87
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-40.png pad 40:40
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-40@2x.png pad 80:80
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-40@3x.png pad 120:120
+
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-50.png pad 50:50
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-50@2x.png pad 100:100
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-57.png pad 57:57
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-57@2x.png pad 114:114
+
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-60@2x.png pad 120:120
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-60@3x.png pad 180:180
+
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-72.png pad 72:72
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-72@2x.png pad 114:114
+
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-76.png pad 76:76
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-76@2x.png pad 152:152
+
+call svgexport icon.svg App_Resources/iOS/Assets.xcassets/AppIcon.appiconset/icon-83.5@2x.png pad 167:167
+
+
+
